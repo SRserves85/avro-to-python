90904dfcb9befdad837f31030bdd1dbe31a0615e
srserves85 <srserves85@gmail.com>
2020-01-09 14:08:04 -0500

    bump version

--- test-avro/test_avro/records/nested/__init__.py
+++ test-avro/test_avro/records/nested/__init__.py
@@ -1,6 +1,6 @@
 """ init file for file imports at namespace level """
-from test_avro.records.nested.Flavor import Flavor
+from test_avro.records.nested.Thing2 import Thing2
 
 from test_avro.records.nested.Thing3 import Thing3
 
-from test_avro.records.nested.Thing2 import Thing2
+from test_avro.records.nested.Flavor import Flavor
