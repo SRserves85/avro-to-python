bd69fdba089470fd7995b6d03eda67575a99507d
srserves85 <srserves85@gmail.com>
2020-02-06 14:23:21 -0500

    github sucks

--- test-avro/test_avro/records/__init__.py
+++ test-avro/test_avro/records/__init__.py
@@ -1,16 +1,16 @@
 """ init file for file imports at namespace level """
-from test_avro.records.RecordWithRecord import RecordWithRecord
+from test_avro.records.ComplexRecord import ComplexRecord
 
 from test_avro.records.RecordWithEnum import RecordWithEnum
 
-from test_avro.records.RecordWithLogicalTypes import RecordWithLogicalTypes
-
 from test_avro.records.Thing import Thing
-
-from test_avro.records.RecordWithArray import RecordWithArray
 
 from test_avro.records.ExampleEvent import ExampleEvent
 
+from test_avro.records.RecordWithArray import RecordWithArray
+
+from test_avro.records.RecordWithLogicalTypes import RecordWithLogicalTypes
+
 from test_avro.records.RecordWithUnion import RecordWithUnion
 
-from test_avro.records.ComplexRecord import ComplexRecord
+from test_avro.records.RecordWithRecord import RecordWithRecord
