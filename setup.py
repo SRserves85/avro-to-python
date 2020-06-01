2e17483f774c8b4f575914de32aebb7c13f3aa04
srserves85 <srserves85@gmail.com>
2020-06-01 13:04:47 -0400

    version bump to 0.2.3

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/srserves85/avro-to-python',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
