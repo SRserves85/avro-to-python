c36356400e5a4618b03e8b2de78f51cf3aee70fd
Jonathan Leung <jonathan.leung@storyblocks.com>
2022-03-17 16:35:04 -0400

    setup requirements?

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Jinja2==2.10.3', 'Click==7.0']
+requirements = ['Jinja2==3.0.3', 'Click==7.0']
 
 test_requirements = [
     'pip==19.2.3',
@@ -26,7 +26,7 @@ test_requirements = [
     'Click==7.0',
     'pytest==4.6.5',
     'pytest-runner==5.1',
-    'Jinja2==2.10.3'
+    'Jinja2==3.0.3'
 ]
 
 setup(
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/srserves85/avro-to-python',
-    version='0.3.3',
+    version='0.4.0',
     zip_safe=False,
 )
